de361d373c60f04f91fbc83ebfcc45d74c242ac7
Rod Ta <rody101@gmail.com>
2023-01-27 00:04:35 -0500

    Created express API project

---
+++ nx-pokemon-1/apps/api/src/main.ts
@@ -0,0 +1,34 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from 'express';
+import * as path from 'path';
+
+import { pokemon } from './pokemon';
+
+const app = express();
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to Rod!' });
+});
+
+app.get('/pokemon', (_, res) => {
+  res.send(pokemon);
+});
+
+app.get('/search', (req, res) => {
+  const q = ((req.query.q as string) ?? '').toLowerCase();
+  res.send(
+    pokemon.filter(({ name: { english } }) => english.toLowerCase().includes(q))
+  );
+});
+
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
