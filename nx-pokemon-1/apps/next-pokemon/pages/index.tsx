4da91679491ccdab9c82623d759b2d915b1fd51a
Rod Ta <rody101@gmail.com>
2023-01-27 01:29:13 -0500

    changed to SSR

--- nx-pokemon-1/apps/next-pokemon/pages/index.tsx
+++ nx-pokemon-1/apps/next-pokemon/pages/index.tsx
@@ -3,9 +3,11 @@ import type { Pokemon } from '@nx-pokemon-1/shared-types'
 
 import styles from './index.module.css';
 
-export function Index() {
-  const [search, setSearch] = useState('')
-  const [pokemon, setPokemon] = useState<Pokemon[]>([])
+export function Index({ q, pokemon: initialPokemon }: { q: string; pokemon: Pokemon[] }) {
+  const [search, setSearch] = useState(q)
+  const [pokemon, setPokemon] = useState<Pokemon[]>(initialPokemon)
+
+  console.log(initialPokemon);
 
   useEffect(() => {
     fetch(`http://localhost:3333/search?q=${search}`)
@@ -27,6 +29,20 @@ export function Index() {
       </ul>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  let pokemon = [];
+  if (context.query.q) {
+    const res = await fetch(`http://localhost:3333/search?q=${context.query.q}`);
+    pokemon = await res.json();
+  }
+  return {
+    props: {
+      q: context.query.q ?? '',
+      pokemon
+    },
+  }
 }
 
 export default Index;
