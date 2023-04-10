4d878c7be22525a9456f6ebfc69931a2f6adb5ec
Rod Ta <rody101@gmail.com>
2023-04-10 17:50:27 -0400

    Added basic client side logging test

--- nx-pokemon-1/apps/api/src/main.ts
+++ nx-pokemon-1/apps/api/src/main.ts
@@ -7,6 +7,11 @@ import { pokemon } from './pokemon';
 const app = express();
 app.use(cors());
 
+var JL = require('jsnlog').JL;
+var jsnlog_nodejs = require('jsnlog-nodejs').jsnlog_nodejs;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
@@ -22,6 +27,20 @@ app.get('/search', (req, res) => {
   res.send(
     pokemon.filter(({ name: { english } }) => english.toLowerCase().includes(q))
   );
+});
+
+JL().info('info message!!!!!!!!!Asd!');
+
+// jsnlog.js on the client by default sends log messages to jsnlog.logger, using POST.
+// app.post('*.logger', function (req, res) {
+app.post('/logger', (req, res) => {
+  // Process incoming log messages, by handing to the server side jsnlog.
+  // JL is the object that you got at
+  // var JL = require('jsnlog').JL;
+  jsnlog_nodejs(JL, req.body);
+
+  // Send empty response. This is ok, because client side jsnlog does not use response from server.
+  res.send('');
 });
 
 const port = process.env.PORT || 3333;
--- nx-pokemon-1/apps/next-pokemon/pages/index.tsx
+++ nx-pokemon-1/apps/next-pokemon/pages/index.tsx
@@ -2,12 +2,23 @@ import React, { useEffect, useState, useCallback } from 'react';
 import type { Pokemon } from '@nx-pokemon-1/shared-types'
 
 import styles from './index.module.css';
+import { JL } from 'jsnlog';
+
+JL.setOptions({
+  "defaultAjaxUrl": "http://localhost:3333/logger"
+});
 
 export function Index({ q, pokemon: initialPokemon }: { q: string; pokemon: Pokemon[] }) {
   const [search, setSearch] = useState(q)
   const [pokemon, setPokemon] = useState<Pokemon[]>(initialPokemon)
 
-  console.log(initialPokemon);
+  // console.log(initialPokemon);
+
+  const handleOnClick = () => {
+    console.log("click");
+    JL('client').info('message from the client');
+    JL('client').debug({ x: 5, y: 7 });
+  }
 
   useEffect(() => {
     fetch(`http://localhost:3333/search?q=${search}`)
@@ -21,7 +32,9 @@ export function Index({ q, pokemon: initialPokemon }: { q: string; pokemon: Poke
 
   return (
     <div className={styles.page}>
+      Search:
       <input value={search} onChange={onSetSearch} />
+      <button onClick={handleOnClick}>test</button>
       <ul>
         {pokemon.map(({ name: { english } }, id) => (
           <li key={id}>{english}</li>
