a51f4d06b2ed0dac3ac412036b9302a60442965a
Rod Ta <rody101@gmail.com>
2023-04-11 14:56:04 -0400

    Added more logging examples

--- nx-pokemon-1/apps/next-pokemon/pages/index.tsx
+++ nx-pokemon-1/apps/next-pokemon/pages/index.tsx
@@ -3,21 +3,50 @@ import type { Pokemon } from '@nx-pokemon-1/shared-types'
 
 import styles from './index.module.css';
 import { JL } from 'jsnlog';
+import { ErrorBoundary } from "react-error-boundary";
 
 JL.setOptions({
-  "defaultAjaxUrl": "http://localhost:3333/logger"
+  "defaultAjaxUrl": "http://localhost:3333/logger",
+  requestId: "requestID_1!!",
 });
+
+const data: any = { sid: 'D12345', appVersion: '1' }
+
+var logger = JL("LoggerName@@@@ - " + JSON.stringify(data));
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  // Call resetErrorBoundary() to reset the error boundary and retry the render.
+
+  return (
+    <div role="alert">
+      <p>Something went wrong!!!!!!!!:</p>
+      <pre style={{ color: "red" }}>{error.message}</pre>
+    </div>
+  );
+}
 
 export function Index({ q, pokemon: initialPokemon }: { q: string; pokemon: Pokemon[] }) {
   const [search, setSearch] = useState(q)
   const [pokemon, setPokemon] = useState<Pokemon[]>(initialPokemon)
+  const [showErrorPage, setShowErrorPage] = useState<boolean>(false);
 
   // console.log(initialPokemon);
 
+  const logError = (error: Error, info: { componentStack: string }) => {
+    // Do something with the error, e.g. log to an external API
+    // JL('client ERROR!!').error(error);
+    // JL('client INFO!!').error(info);
+    // JL('client FATAL!!').fatalException("Exception was thrown!", info);
+    // JL('client end!!').info("end!!!!!!!@@#");
+    logger.fatalException("Exception was thrown!", info);
+    logger.info("end!!!!!!!@@#");
+  };
+
   const handleOnClick = () => {
-    console.log("click");
-    JL('client').info('message from the client');
-    JL('client').debug({ x: 5, y: 7 });
+    // JL('clientABC@').info('message from the client');
+    // JL('client').debug({ x: 5, y: 7 });
+    // logger.info("test!!!!!!!");
+    setShowErrorPage(true);
   }
 
   useEffect(() => {
@@ -30,17 +59,38 @@ export function Index({ q, pokemon: initialPokemon }: { q: string; pokemon: Poke
     setSearch(evt.target.value);
   }, []);
 
+  const ButtonComponentForceError = () => {
+    throw Error("error!");
+    return <></>;
+  };
+
   return (
-    <div className={styles.page}>
-      Search:
-      <input value={search} onChange={onSetSearch} />
-      <button onClick={handleOnClick}>test</button>
-      <ul>
-        {pokemon.map(({ name: { english } }, id) => (
-          <li key={id}>{english}</li>
-        ))}
-      </ul>
-    </div>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onError={logError}
+    >
+      {showErrorPage ? (<ButtonComponentForceError />) : (<div className={styles.page}>
+        Search:
+        <input value={search} onChange={onSetSearch} />
+        <button onClick={handleOnClick}>test</button>
+        <ul>
+          {pokemon.map(({ name: { english } }, id) => (
+            <li key={id}>{english}</li>
+          ))}
+        </ul>
+      </div>)}
+      {/* <ButtonComponent /> */}
+      {/* <div className={styles.page}>
+        Search:
+        <input value={search} onChange={onSetSearch} />
+        <button onClick={handleOnClick}>test</button>
+        <ul>
+          {pokemon.map(({ name: { english } }, id) => (
+            <li key={id}>{english}</li>
+          ))}
+        </ul>
+      </div> */}
+    </ErrorBoundary >
   );
 }
 
